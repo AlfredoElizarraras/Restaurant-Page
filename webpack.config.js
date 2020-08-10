b9bb979cb6552ae1e58bbbf5684ef8714647319d
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-08-10 16:50:43 -0500

    Add configuration files: package.json, webpack.config.js.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+	  filename: 'main.js',
+	  path: path.join(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+    ]
+  }
+};
