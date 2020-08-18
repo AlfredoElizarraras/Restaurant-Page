b52dca0cb8ab44159d0b22826855ceda70daf7b2
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-08-17 21:29:34 -0500

    Fix linter errors.

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: "./src/index.js",
+    app: './src/index.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
     ],
   },
-  mode: "development",
+  mode: 'development',
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Restaurant page",
+      title: 'Restaurant page',
     }),
   ],
   output: {
-    filename: "[name].main.js",
-    path: path.join(__dirname, "dist"),
+    filename: '[name].main.js',
+    path: path.join(__dirname, 'dist'),
   },
 };
