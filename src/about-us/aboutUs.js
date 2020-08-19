f320c997dd92e2a9850a8849fecf0fb97e8a0491
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-08-18 22:14:44 -0500

    Refactor about us page to add elements only with DOM manipulations.

--- src/about-us/aboutUs.js
+++ src/about-us/aboutUs.js
@@ -1,27 +1,46 @@
 import './aboutUsStyles.css';
-import aboutUsImage from '../images/about-us.jpg';
+import aboutUsImageSrc from '../images/about-us.jpg';
 
 const AboutUsPage = (() => {
-  const createAboutUsPageMarkUp = () => `
-    <div id="aboutUsId" class="about-us page d-none">
-    <article class="about-us__page">
-      <img id="about-us-image"/>
-      
-      <div class="about-us__info">
-        <h2 class="about-us__title">About us</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aliquid qui sint placeat tempora aperiam ducimus, odit cum omnis expedita minus accusantium! Debitis fugiat numquam officia eveniet nostrum ut et.</p>
-      </div>
-    </article>
-  </div>
-    `;
+  const createAboutUsPageElement = () =>{
+    // create returning element and child elements
 
-  const addImage = () => {
-    document.getElementById('about-us-image').src = aboutUsImage;
+    const aboutUsIdContainer = document.createElement('div');
+    const aboutUsArticle = document.createElement('article');
+    const aboutUsImage = document.createElement('img');
+    const aboutUsInfo = document.createElement('div');
+    const aboutUsTitle = document.createElement('h2');
+    const aboutUsDescription = document.createElement('p');
+
+    // assign id's
+    aboutUsIdContainer.id = 'aboutUsId';
+    aboutUsImage.id = 'about-us-image';
+
+    // assign classes
+    aboutUsIdContainer.classList.add('about-us');
+    aboutUsIdContainer.classList.add('page');
+    aboutUsIdContainer.classList.add('d-none');
+    aboutUsArticle.classList.add('about-us__page');
+    aboutUsInfo.classList.add('about-us__info');
+    aboutUsTitle.classList.add('about-us__title');
+
+    // assign values
+    aboutUsTitle.innerHTML = 'About Us';
+    aboutUsDescription.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aliquid qui sint placeat tempora aperiam ducimus, odit cum omnis expedita minus accusantium! Debitis fugiat numquam officia eveniet nostrum ut et.';
+    aboutUsImage.src = aboutUsImageSrc;
+
+    // append elements to containers
+    aboutUsInfo.append(aboutUsTitle);
+    aboutUsInfo.append(aboutUsDescription);
+    aboutUsArticle.append(aboutUsImage);
+    aboutUsArticle.append(aboutUsInfo);
+    aboutUsIdContainer.append(aboutUsArticle);
+
+    return aboutUsIdContainer;
   };
 
   const renderAboutUsPage = (parent) => {
-    document.getElementById(parent).innerHTML += createAboutUsPageMarkUp();
-    addImage();
+    document.getElementById(parent).appendChild(createAboutUsPageElement());
   };
 
   return {
